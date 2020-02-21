306f22d64a59c54a5fbce03f98abff2f9679c54a
Tania <taniafernandes98@gmail.com>
2020-02-21 16:58:18 -0300

    operação de soma funcionando

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main() {
-	int i;
-	float f;
-	for (i=1, f=1 /*lista*/; i<=14/*bool*/; i++) {
-		f = f * i;
-		printf("\nfatorial de %d = %.0f", i, f);
+	char op;
+	int operando1, operando2;
+	printf("Qual operação realizar?\n");
+	scanf("%c", &op); //operador de extração de endereço
+	printf("Qual o primeiro operando?\n");
+	scanf("%d", &operando1);
+	printf("Qual o segundo operando?\n");
+	scanf("%d", &operando2);
+	switch (op){
+		case '+':
+			printf("%d + %d = %d\n", operando1, operando2, operando1+operando2);
+			break;
 	}
 	return 0;
 }
